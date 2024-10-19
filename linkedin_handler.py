023148e7345dccfe62a6e6e0171b40aaf7e81b73
leofaria2005 <126530042+leofaria2005@users.noreply.github.com>
2024-10-19 05:50:14 +0100

    a

--- linkedin_handler.py
+++ linkedin_handler.py
@@ -1,35 +1,144 @@
-from linkedin_api.clients.restli.client import RestliClient
+import requests
+import os
 import json
+import webbrowser
+
+from dotenv import load_dotenv
+
+#load file into env object
+load_dotenv('access_token.env')
+#access_token = os.getenv('ACCESS_TOKEN')
+
+
+api_url = 'https://www.linkedin.com/oauth/v2' 
+
+
 
 
 class LinkedinHandler:
-    ACCESS_TOKEN = "AQVW3empMe6mMLhSfp4fP4CQuW3307XWSNMX6f2tyT91TjXUC0eAMBhUtbgO1I_SLRdg1HL59FWFf6d185X2u34f0Ius5-CYFbZ6bkWBXWFZ4W0_nDYpyOTqKmG9Jk38JVwKpKd-yC35-PFjOXglcPr5VMQ7E7LzD5RSUsZnShjZhyvG3RtsO5U8gbbljoa7BR_y5HvzJJTvYC5lmgkOEmYx0JOFVfGGXOOcnf10exy4tui5RIy6e5wfPoQy_ins9mta1W90i5EecPIJ5ci3VEjLWWtl5z-Amw0CLcqp2f8qLjshYyjvRb-y_l-WQrzz4BCzunze1KFrUT35C5RnVSo95XT20A"
-    ME_RESOURCE = "/me"
-    UGC_POSTS_RESOURCE = "/ugcPosts"
-    POSTS_RESOURCE = "/posts"
-    API_VERSION = "202302"
+
+    __auth_code = ''
+    __access_token = ''
+    __headers= ''
+    __user_info = ''
 
     def __init__(self):
-        self.restli_client = RestliClient()
-        self.restli_client.session.hooks["response"].append(lambda r: r.raise_for_status())
+        self.get_auth_code()
+        self.get_access_token()
+        self.get_headers()
+        self.get_user_info()
 
-    def post(self, text):
-        me_ID = self.restli_client.get(resource_path=self.ME_RESOURCE, access_token=self.ACCESS_TOKEN)
-        print(f"Successfully fetched profile: {json.dumps(me_ID.entity)}")
-        posts_create_response = self.restli_client.create(
-            resource_path=self.POSTS_RESOURCE,
-            entity={
-              "author": f"urn:li:person:{me_ID.entity['id']}",
-              "visibility": "PUBLIC",
+    def read_credentials(self,file):
+        #API credentials are stored in a safe place
 
-              "commentary": text,
-              "distribution": {
-                   "feedDistribution": "MAIN_FEED",
-                   "targetEntities": [],
-                   "thirdPartyDistributionChannels": [],
-              },
+        with open(file) as f:
+            credentials = json.load(f)
+        return credentials
+    
+    def parse_redirect_uri(self, redirect_response):
+        '''
+        Parse redirect response into components.
+        Extract the authorized token from the redirect uri.
+        '''
+        from urllib.parse import urlparse, parse_qs
+
+        url = urlparse(redirect_response)
+        url = parse_qs(url.query)
+        return url['code'][0]
+    
+    def save_token(self, filename,data):
+        '''
+        Write token to credentials file.
+        '''
+        data = json.dumps(data, indent = 4) 
+        with open(filename, 'w') as f: 
+            f.write(data)
+
+    def get_auth_code(self):
+        #Make a HTTP request to the server
+        #Once authorized, will redirect to redirect_uri
+
+        credentials = self.read_credentials('credentials.json')
+        client_id, client_secret = credentials['client_id'], credentials['client_secret']
+        redirect_uri = credentials['redirect_uri']
+        
+        params = {
+            'response_type': 'code',
+            'client_id': client_id,
+            'redirect_uri': redirect_uri,
+            'scope': 'w_member_social openid email profile'
+        }
+
+        response = requests.get(f'{api_url}/authorization', params=params)
+
+        webbrowser.open(response.url)
+
+        auth_code = input("Past code: ")
+        self.__auth_code = auth_code
+
+
+    def get_access_token(self):
+
+        credentials = self.read_credentials('credentials.json')
+        client_id, client_secret = credentials['client_id'], credentials['client_secret']
+        redirect_uri = credentials['redirect_uri']
+
+        headers= {'Content-Type': 'application/x-www-form-urlencoded'}
+
+        params = {
+            'grant_type': 'authorization_code',
+            'code': f'{self.__auth_code}',
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'redirect_uri': redirect_uri
+        }
+
+        response = requests.post(f'{api_url}/accessToken', headers=headers, params=params)
+        token = response.json()
+        self.__access_token = token['access_token']
+
+        
+
+
+    def get_headers(self):
+        #Make headers to attach to the API call
+
+        headers = {
+            'Authorization': f'Bearer {self.__access_token}',
+            'cache-control': 'no-cache',
+            'X-Restli-Protocol-Version': '2.0.0'
+        }
+        self.__headers = headers
+        
+
+    def get_user_info(self):
+        response = requests.get('https://api.linkedin.com/v2/userinfo', headers = self.__headers)
+        user_info = response.json()
+        self.__user_info = user_info
+        
+
+         
+
+    def post(self, message):
+
+        post_data = {
+            'author': f'urn:li:person:{self.__user_info['sub']}',
+            'lifecycleState': 'PUBLISHED',
+            'specificContent': {
+                'com.linkedin.ugc.ShareContent': {
+                    'shareCommentary': {
+                        'text': message
+                    },
+                    'shareMediaCategory': 'NONE'
+                }
             },
-           #version_string=API_VERSION,
-            access_token=self.ACCESS_TOKEN,
-        )
-        print(f"Successfully created post using /posts: {posts_create_response.entity_id}")
+            'visibility': {
+                'com.linkedin.ugc.MemberNetworkVisibility': 'CONNECTIONS'
+            }
+        }
+
+        response = requests.post('https://api.linkedin.com/v2/ugcPosts', headers=self.__headers, json=post_data)
+        print(response.json())
+        
+    
+    
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from gpt_handler import GPTHandler
 from linkedin_handler import LinkedinHandler
+import requests
+
 
 linkedin_handler = LinkedinHandler()
 gpt_handler = GPTHandler()
+message = input("Prompt: ")
 
-answer = gpt_handler.ask_question("The event was thew AWS summit yesterday, it gave me opportunities to network and learn about AWS")
-
+answer = gpt_handler.ask_question(message)
 print(answer)
 
 linkedin_handler.post(answer)
+#print(answer)
+
+
 
